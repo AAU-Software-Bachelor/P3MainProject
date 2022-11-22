31a390a8998a020f667efae42c0a4aa024b30bee
anderszeic <anderszeic@gmail.com>
2022-11-22 10:37:33 +0100

    fixed types again

--- Project2/classes/characterTrait2.cs
+++ Project2/classes/characterTrait2.cs
@@ -6,11 +6,16 @@ class characterTrait
 	
 	public characterTrait(string Name, string Image, string Desciption, int uid)
 	{
-		string name  = Name;
-		string image = Image;
-		string description = Desciption;
-		int UID = uid; //1**** for misc 2**** for resouce 3**** for ability 4**** for race
+		name  = Name;
+		image = Image;
+		description = Desciption;
+		UID = uid; //1**** for misc 2**** for resouce 3**** for ability 4**** for race
 	}
+
+	public string name;
+	public string image;
+	public string description;
+	public int UID;
 
 	public void DeleteTrait()
 	{ 
@@ -30,16 +35,16 @@ class characterTrait
 class majorTrait : characterTrait
 {
 	public majorTrait(string Name, string Image, string Desciption, int uid,
-					  List<string> Exclusions, int Type, List<string> Dependencies, List<string> Discounts, int Cost, List<string> AffectedResources, List<int> FreeAbilities)
+					  List<string> Exclusions, int Type, List<string> Dependencies, List<string> Discounts, int Cost, List<string> AffectedResources, List<int> FreeAbilities, int Costtype)
 					  : base(Name, Image, Desciption, uid)
 	{
 		int costType = Costtype;
 		int cost = Cost;
 		int type = Type;
 		object dependentDiscount = new { };
-		List<string> exclusions = new List<string>();
-		List<string> AffectedResources = new List<string>();
-		List<int> FreeAbilities = new List<int>();
+		exclusions = new List<string>();
+		affectedResources = new List<string>();
+		freeAbilities = new List<int>();
 	}
 	public List<string> exclusions {get; set;}
 	public int type {get; set;}
@@ -48,13 +53,14 @@ class majorTrait : characterTrait
 	public int cost {get; set;}
 	public List<string> affectedResources = new List<string>();
 	public List<int> freeAbilities = new List<int>();
+	public int Costtype {get; set;}
 
 	public void addAffectedResources(string uid, int ammount){
-		this.AffectedResources.Add(uid);
+		this.affectedResources.Add(uid);
 	}
-	public void addFreeAbilities(string uid)
+	public void addFreeAbilities(int UID)
 	{
-		this.FreeAbilities.Add(uid);
+		this.freeAbilities.Add(UID);
 	}
 	public void addExclusions(string uid)
 	{
