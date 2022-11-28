849c6abb38a68e987e0bc4a58170763d37fb44de
nightvobs <74359672+nightvobs@users.noreply.github.com>
2022-11-28 10:17:13 +0100

    test Json

--- Project2/classes/ConfigClass.cs
+++ Project2/classes/ConfigClass.cs
@@ -18,6 +18,30 @@ namespace Project2
 		public List<majorTrait[]> MTList = new List<majorTrait[]>() ; //inaccesible in race menu due to its protection level
 		resource[] ResArr = new resource[100];
 
+		public void TestWriteToJson(string fileName)
+        {
+			majorTrait TestMT = new majorTrait(1-01);
+			TestMT.name = "SMITE";
+			TestMT.description = "slå gud modstander med hellig styrke"
+			TestMT.Type = "Ability";
+			TestMT.cost = 4;
+			TestMT.CostTypes = new list<string>() { "KrigerXP", "præstXP"};
+			TestMT.dependecy = new List<List<string>>()
+            {
+				new List<string>(){"kriger", "præst"}
+				new List<string>(){"vildt slag", "hellig slag"}
+            };
+			TestMT.freeAbilities = new List<string>() {"hellig inderlighed"};
+			TestMT.addDiscount("2-01");
+			TestMT.discounts[0].Amount = 2;
+			TestMT.discounts[0].CostUID.add("r-04");
+			TestMT.discounts[0].CostUID.add("r-05");
+			TestMT.addAffectedResources("r-01",2);
+
+			string jsonString = JsonSerializer.Serialize(TestMT);
+			File.WriteAllText(fileName, jsonString);
+        }
+
 		public int getUID(string type)
 		{
 			if (type == "r")
@@ -42,6 +66,7 @@ namespace Project2
 			}
 			return -1;
 		}
+
 		bool saveToList(majorTrait trait)
 		{
 			string[] id = trait.UID.Split('-');
